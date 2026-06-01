6ccd5a2b736fa0273b59056a3b04f0cb604b1489
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-06-01 14:57:48 +0900

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //変数(int = 整数)
+    int codingSkill = 95;
+    //分岐点
+    if(codingSkill >= 80){
+        cout << "大丈夫です" << endl;
+    }else {
+        cout << "練習が必要です" << endl;
+    }
+    return 0;
+}
